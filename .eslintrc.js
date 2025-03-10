1b250c3073138d043546ad68fcf9f20dea43668f
sergi <sergi.font.jane@estudiantat.upc.edu>
2025-03-10 13:41:59 +0100

    reorganized folders and modified eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,17 @@ module.exports = {
   extends: ['expo', 'prettier'],
   plugins: ['prettier'],
   rules: {
-    'prettier/prettier': 'error',
+    'prettier/prettier': 'error', // Marca como error cualquier inconsistencia con las reglas de Prettier
+    'react/react-in-jsx-scope': 'off', // No necesario en proyectos Expo/React Native
+    'no-unused-vars': 'warn', // Puedes ajustar a 'error' si prefieres ser más estricto
+    'react/jsx-curly-brace-presence': [
+      'warn',
+      { props: 'never', children: 'never' },
+    ], // Evita llaves innecesarias en JSX
   },
-  ignorePatterns: ['/dist/*'],
+  env: {
+    browser: true,
+    node: true,
+  },
+  ignorePatterns: ['/dist/*', 'node_modules'],
 };
