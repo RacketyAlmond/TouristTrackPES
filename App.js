d0b3b46b79e9f9fefccaea6018efd36c0ca6ec24
alejandro.comadran <alejandro.comadran@estudiantat.upc.edu>
2025-03-18 13:11:29 +0100

    'filtros v2'

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
+/* eslint-disable prettier/prettier */
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Filtro from './components/organisms/filtro';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style='auto' />
+      <Filtro />
     </View>
   );
 }
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
