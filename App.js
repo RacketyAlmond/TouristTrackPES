99a37e514938a2f459965b2bb4563df0f2ac443c
sergi <sergi.font.jane@estudiantat.upc.edu>
2025-04-24 11:47:30 +0200

    initial screen map

--- App.js
+++ App.js
@@ -25,6 +25,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
+        initialRouteName='Mapa' // Establecer el mapa como pantalla inicial
         screenOptions={{
           gestureEnabled: true,
           gestureDirection: 'horizontal',
